ad77a980bca0d83f9e790303c3ac2cf01310763d
Eder <ederscuer@hotmail.com>
2017-10-30 15:37:45 -0200

    Aprofundamento em rotas de autenticação

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function(){
+	$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+	$this->post('login', 'Auth\LoginController@login');
+	$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+	$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	$this->get('password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm')->name('password.reset');
+	$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+});
