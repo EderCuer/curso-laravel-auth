d991642fb0b01944ac4738d65c0b155fe81ed6fc
Eder <ederscuer@hotmail.com>
2017-11-08 10:49:40 -0200

    Autorização

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
 	$this->get('password/reset/{token}', 'Auth\ForgotPasswordController@showResetForm')->name('password.reset');
 	$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
-	Route::get('/home', 'HomeController@index')->name('home');
+	Route::group(['middleware' => 'can:admin'], function(){
+		Route::get('/home', 'HomeController@index')->name('home');
+	});
+
 });
